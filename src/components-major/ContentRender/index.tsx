38cc2877f663fa6f2dbec65a21d90e718761ee9e
Emad-Armoun <emad.armoun@deliveryhero.com>
2023-07-02 00:37:05 +0200

    make main view conditional

--- src/components-major/ContentRender/index.tsx
+++ src/components-major/ContentRender/index.tsx
@@ -2,14 +2,28 @@
 import { BodyDiv } from './styled';
 import { Button } from '@deliveryhero/armor';
 import { StarFilledIcon } from '@deliveryhero/armor-icons';
+import LinksPage from '../LinksPage';
 
 const ContentRender = () => {
+  const menuNumber = 1;
+
+  const renderPage = () => {
+    switch (menuNumber) {
+      case 1:
+        return <LinksPage />;
+      default:
+        return (
+          <Button onClick={() => alert("you clicked!")}>
+            <StarFilledIcon />
+            Click me
+          </Button>
+        )
+    }
+  };
+
   return (
     <BodyDiv>
-      <Button onClick={() => alert("you clicked!")}>
-        <StarFilledIcon />
-        Click me
-      </Button>
+      { renderPage() }
     </BodyDiv>
   )
 }
