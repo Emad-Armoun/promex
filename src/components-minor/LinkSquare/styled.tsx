9c54a148c26fd9a4f81cbfb22dfc2670f8f0a03e
Emad-Armoun <emad.armoun@deliveryhero.com>
2023-07-02 00:52:35 +0200

    improve UI

--- src/components-minor/LinkSquare/styled.tsx
+++ src/components-minor/LinkSquare/styled.tsx
@@ -1,11 +1,12 @@
 import { styled } from "@deliveryhero/armor";
 
 export const SquareDiv = styled.div`
-  width: 170px;
-  height: 190px;
-  background-color: white;
-  border: 1px solid #CCC;
-  border-radius: 6px;
+  width: 140px;
+  height: 170px;
+  background-color: #EEE;
+  background: linear-gradient(0deg, rgba(235,235,235,1) 0%, rgba(255,255,255,1) 50%, rgba(235,235,235,1) 100%);
+  border: none;
+  // border-radius: 10px;
 
   a {
     display: flex;
