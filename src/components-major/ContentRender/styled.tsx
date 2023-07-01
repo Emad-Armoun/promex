32f49b67e903b240b22b7a27ca5ea1569c7deefd
Emad-Armoun <emad.armoun@deliveryhero.com>
2023-07-02 00:37:22 +0200

    remove old style to center the main contents

--- src/components-major/ContentRender/styled.tsx
+++ src/components-major/ContentRender/styled.tsx
@@ -4,7 +4,5 @@ export const BodyDiv = styled.div`
   height: calc(100% - 75px - 20px - 20px);
   overflow-y: scroll;
   padding: 20px;
-  display: flex;
-  justify-content: center;
   background-color: #FFF;
 `;
