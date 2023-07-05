339bc504e9cbe2e7150eec94879532e73bba9f5b
Emad-Armoun <emad.armoun@deliveryhero.com>
2023-07-05 14:29:44 +0200

    Add hover animation

--- src/components-minor/LinkSquare/index.tsx
+++ src/components-minor/LinkSquare/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { SubItem } from "../../types/links";
 import { SquareDiv } from "./styled";
 
@@ -6,8 +7,14 @@ type Props = {
 }
 
 const LinkSquare: React.FC<Props> = ({ item }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <SquareDiv>
+    <SquareDiv
+      onMouseOver={() => setIsHovered(true)}
+      onMouseOut={() => setIsHovered(false)}
+      className={isHovered ? 'over' : 'out'}
+    >
       <a href={item.link} target="_blank">
         <img src={item.icon} alt={item.title} />
         <h3>{item.title}</h3>
--- src/components-minor/LinkSquare/styled.tsx
+++ src/components-minor/LinkSquare/styled.tsx
@@ -4,8 +4,43 @@ export const SquareDiv = styled.div`
   width: 140px;
   height: 160px;
   background-color: #EEE;
-  background: linear-gradient(0deg, rgba(235,235,235,1) 0%, rgba(255,255,255,1) 25%, rgba(235,235,235,1) 100%);
+  background: linear-gradient(0deg, #ccc, #fff, #fff, #eee, #fff, #eee);
+  // background: linear-gradient(0deg, #ccc, #fff, #ccc, #eee, #fff, #eee);
+  // background: linear-gradient(0deg, #ccc, #eee, #fff, #eee, #fff, #eee);
+  // background: linear-gradient(0deg, #eee, #fff, #eee, #ddd, #eee, #f5f5f5);
+	background-size: 100% 200%;
   border: none;
+
+  @keyframes slide-down {
+    0% {
+      background-position: 0% 0%;
+    }
+    100% {
+      background-position: 0% 100%;
+    }
+  }
+
+  @keyframes slide-up {
+    0% {
+      background-position: 0% 100%;
+    }
+    100% {
+      background-position: 0% 0%;
+    }
+  }
+
+  &.over {
+    animation: slide-down 0.5s ease;
+	  animation-fill-mode: forwards;
+
+    img {
+      scale: 1.1;
+    }
+  }
+
+  &.out {
+    animation: slide-up 1s ease;
+  }
 
   a {
     display: flex;
@@ -17,6 +52,7 @@ export const SquareDiv = styled.div`
     img {
       height: 70px;
       margin-bottom: 10px;
+      transition: all 0.5s ease-in-out;
     }
 
     h3 {
