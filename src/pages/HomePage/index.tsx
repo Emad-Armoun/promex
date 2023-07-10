bc8e9b90896c9781a7c734df7911cbcc034e39c0
Emad-Armoun <emad.armoun@deliveryhero.com>
2023-07-10 23:51:14 +0200

    Show the favorited links in the home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { Links } from "../../components-major/Links";
 import { ContainerDiv } from "./styled"
+import { FAVORITE_ITEMS_KEY } from "../../util/constants";
+import { SpecialCategory, SpecialLink } from "../../types/links";
+import data from '../../data/links.json';
 
 export const HomePage: React.FC = () => {
-  return <ContainerDiv><h1>Home Page</h1></ContainerDiv>
+  const [favoriteItems, setFavoriteItems] = useState<SpecialCategory[]>([]);
+
+  useEffect(() => {
+    const storedFavoriteItemIds = localStorage.getItem(FAVORITE_ITEMS_KEY);
+    if (storedFavoriteItemIds) {
+      const favoriteItemIds = JSON.parse(storedFavoriteItemIds);
+
+      const filteredData: SpecialCategory[] = [];
+      for (const category of data.items) {
+        const filteredCategoryItems: SpecialLink[] = category.subItems.filter(item => favoriteItemIds.includes(item.id));
+        if (filteredCategoryItems.length > 0) {
+          filteredData.push({
+            title: category.title,
+            icon: category.icon,
+            subItems: filteredCategoryItems
+          });
+        }
+      }
+      setFavoriteItems(filteredData);
+    }
+  }, []);
+
+  return <ContainerDiv>
+    <h1>Home Page</h1>
+    <Links items={favoriteItems} />
+  </ContainerDiv>
 };
