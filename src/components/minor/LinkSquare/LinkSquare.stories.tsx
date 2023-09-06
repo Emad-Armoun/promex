8008f157c19e0af2153a6f3a561dd3806b7832fe
Emad-Armoun <emad.armoun@deliveryhero.com>
2023-09-06 12:25:48 +0200

    add initial stories

---
+++ src/components/minor/LinkSquare/LinkSquare.stories.tsx
@@ -0,0 +1,83 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import LinkSquare from ".";
+import { SizeValues } from "../../../types/settings";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "PromEx/LinkSquare",
+  component: LinkSquare,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    size: {
+      control: "inline-radio",
+      options: [SizeValues.small, SizeValues.big],
+    },
+  },
+} satisfies Meta<typeof LinkSquare>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const WithFavorite: Story = {
+  args: {
+    item: {
+      id: 1,
+      title: "Miro",
+      iconLinkType: "link",
+      icon: "",
+      link: "https://miro.com/app/dashboard/",
+      description: "A whiteboard tool to draw diagrams",
+    },
+    isInFavorite: true,
+    showFavoriteBtn: true,
+    toggleFavorite: (itemId: number) => {
+      alert(`save id=${itemId} in the database`);
+    },
+    size: SizeValues.big as SizeValues,
+  },
+};
+
+export const WithoutFavorite: Story = {
+  args: {
+    item: {
+      id: 1,
+      title: "Miro",
+      iconLinkType: "link",
+      icon: "",
+      link: "https://miro.com/app/dashboard/",
+      description: "A whiteboard tool to draw diagrams",
+    },
+    isInFavorite: false,
+    showFavoriteBtn: false,
+    toggleFavorite: (itemId: number) => {
+      alert(`save id=${itemId} in the database`);
+    },
+    size: SizeValues.big as SizeValues,
+  },
+};
+
+export const SmallButton: Story = {
+  args: {
+    item: {
+      id: 1,
+      title: "Miro",
+      iconLinkType: "link",
+      icon: "",
+      link: "https://miro.com/app/dashboard/",
+      description: "A whiteboard tool to draw diagrams",
+    },
+    isInFavorite: false,
+    showFavoriteBtn: false,
+    toggleFavorite: (itemId: number) => {
+      alert(`save id=${itemId} in the database`);
+    },
+    size: SizeValues.small as SizeValues,
+  },
+};
---
+++ src/components/minor/LoadingComponent/LoadingComponent.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import LoadingComponent from ".";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+  title: "PromEx/LoadingComponent",
+  component: LoadingComponent,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    speed: { control: { type: "number", min: 10, max: 2000, step: 10 } },
+  },
+} satisfies Meta<typeof LoadingComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {},
+};
+
+export const Wider: Story = {
+  args: {
+    loadingText: "L o a d i n g . . . . .",
+    speed: 50,
+  },
+};
+
+export const Longer: Story = {
+  args: {
+    loadingText: "Please wait more, Thank you for you patience!    ",
+    speed: 100,
+  },
+};
